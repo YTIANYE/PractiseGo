3975f55e6ace240d67be9c63e60852af35109e2c
YTIANYE <13293209977@163.com>
2021-12-08 20:36:58 +0800

    [29]剑指 Offer 22. 链表中倒数第k个节点

--- github.com/YTIANYE/Algorithm/main/structure/LinkList/LinkList.go
+++ github.com/YTIANYE/Algorithm/main/structure/LinkList/LinkList.go
@@ -3,7 +3,7 @@ package LinkList
 import "fmt"
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
@@ -11,7 +11,7 @@ func CreatList(nums []int) *ListNode {
 
 	head := &ListNode{}
 	p := head
-	for _,val := range nums{
+	for _, val := range nums {
 		node := &ListNode{Val: val}
 		p.Next = node
 		p = p.Next
@@ -19,19 +19,33 @@ func CreatList(nums []int) *ListNode {
 	return head.Next
 }
 
-func PrintList(head *ListNode){
+func PrintList(head *ListNode) {
 	p := head
 	result := []int{}
-	for p != nil{
+	for p != nil {
 		result = append(result, p.Val)
 		p = p.Next
 	}
 	fmt.Println(result)
 }
 
+func DeleteNode(head *ListNode, val int) *ListNode {
+	prehead := &ListNode{Next: head} // 原链表前加一个头结点
+	p := prehead
+	for p.Next != nil {
+		if p.Next.Val == val {
+			p.Next = p.Next.Next
+			break
+		}
+		p = p.Next
+	}
+	return prehead.Next
+}
+
 // func main(){
 // 	nums := []int{1,2,3}
 // 	list := CreatList(nums)
 // 	PrintList(list)
+// 	list = DeleteNode(list, 2)
+// 	PrintList(list)
 // }
-
