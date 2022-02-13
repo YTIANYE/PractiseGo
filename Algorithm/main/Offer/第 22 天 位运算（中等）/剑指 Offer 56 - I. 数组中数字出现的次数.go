bf90467e0f2ee5b341cde691cc63b4b18dd678c1
YTIANYE <13293209977@163.com>
2022-02-13 22:55:56 +0800

    [52]剑指 Offer 56 - II. 数组中数字出现的次数 II"

--- Algorithm/main/Offer/第 22 天 位运算（中等）/剑指 Offer 56 - I. 数组中数字出现的次数.go
+++ Algorithm/main/Offer/第 22 天 位运算（中等）/剑指 Offer 56 - I. 数组中数字出现的次数.go
@@ -1,1 +1,56 @@
-package 第_22_天_位运算_中等_
+/*
+一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+
+示例 1：
+
+输入：nums = [4,1,4,6]
+输出：[1,6] 或 [6,1]
+示例 2：
+
+输入：nums = [1,2,10,4,1,4,3,3]
+输出：[2,10] 或 [10,2]
+
+
+限制：
+2 <= nums.length <= 10000
+
+*/
+package main
+
+import "fmt"
+
+// 我实现的官方题解
+/*
+执行用时：16 ms, 在所有 Go 提交中击败了84.40%的用户
+内存消耗：6.1 MB, 在所有 Go 提交中击败了99.77%的用户
+*/
+func singleNumbers(nums []int) []int {
+
+	// 找到两个数的异或结果
+	num := nums[0]
+	for i := 1; i < len(nums); i++ {
+		num = num ^ nums[i]
+	}
+	// 异或结果为1的 位
+	temp := 1
+	for temp&num == 0 {
+		temp = temp << 1
+	}
+
+	// 分组
+	num1 := 0
+	num2 := 0
+	for _, n := range nums {
+		if n&temp == 0 {
+			num1 ^= n
+		} else {
+			num2 ^= n
+		}
+	}
+	return []int{num1, num2}
+}
+
+func main() {
+	nums := []int{1, 2, 10, 4, 1, 4, 3, 3}
+	fmt.Println(singleNumbers(nums))
+}
--- Algorithm/main/Offer/第 22 天 位运算（中等）/剑指 Offer 56 - II. 数组中数字出现的次数 II.go
+++ Algorithm/main/Offer/第 22 天 位运算（中等）/剑指 Offer 56 - II. 数组中数字出现的次数 II.go
@@ -1,1 +1,14 @@
-package 第_22_天_位运算_中等_
+/*
+在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+*/
+package main
+
+// 我实现的精选题解
+func singleNumber(nums []int) int {
+	twos, ones := 0, 0
+	for _, num := range nums{
+		ones = ones ^ num & ^twos
+		twos = twos ^ num & ^ones
+	}
+	return ones
+}
