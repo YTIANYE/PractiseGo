e7723edff3cdc2046937df146ab2f36297e28f09
YTIANYE <13293209977@163.com>
2022-02-27 11:25:11 +0800

    [69]剑指 Offer 51. 数组中的逆序对

--- Algorithm/main/Offer/第 30 天 分治算法（困难）/剑指 Offer 17. 打印从1到最大的n位数.go
+++ Algorithm/main/Offer/第 30 天 分治算法（困难）/剑指 Offer 17. 打印从1到最大的n位数.go
@@ -15,13 +15,53 @@ n 为正整数
 */
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
-func printNumbers(n int) []int {
+// 我的题解：暴力解法
+func printNumbers1(n int) []int {
 	res := []int{}
 	end := int(math.Pow(10, float64(n)))
-	for i:=1;i<end;i++{
+	for i := 1; i < end; i++ {
 		res = append(res, i)
 	}
 	return res
+}
+
+// 我的题解：分治
+/*
+执行用时：36 ms, 在所有 Go 提交中击败了7.52%的用户
+内存消耗：13.1 MB, 在所有 Go 提交中击败了5.94%的用户
+*/
+func printNumbers(n int) []int {
+
+	var fenzhi func(string, int) []string
+	fenzhi = func(str string, n int) []string {
+		if n == 0 {
+			return []string{str}
+		}
+		res := []string{}
+		for i := 0; i < 10; i++ {
+			temp := fenzhi(str+strconv.Itoa(i), n-1)
+			res = append(res, temp...)
+		}
+		return res
+	}
+
+	chars := fenzhi("", n)
+	// fmt.Println(chars)
+	res := []int{}
+	for _, str := range chars {
+		num, _ := strconv.Atoi(str)
+		res = append(res, num)
+	}
+	return res[1:]
+}
+
+func main() {
+	n := 2
+	fmt.Println(printNumbers(n))
 }
--- Algorithm/main/Offer/第 30 天 分治算法（困难）/剑指 Offer 51. 数组中的逆序对.go
+++ Algorithm/main/Offer/第 30 天 分治算法（困难）/剑指 Offer 51. 数组中的逆序对.go
@@ -1,1 +1,105 @@
+/*
+在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+
+ 
+
+示例 1:
+
+输入: [7,5,6,4]
+输出: 5
+
+*/
+
 package main
+
+/*
+7	5	6	4
+			0
+		1	0
+	1	1	0
+3
+################
+7	5	6	4
+0
+0	1
+0	1	1
+0	1	1	3
+
+
+
+8	9	7	5	6	4
+					0
+				1
+			1
+		3
+	4
+4
+
+4		0
+64		1
+564		1+1
+7564	1*2+1*2+1
+
+*/
+
+// 暴力方法：超时
+func reversePairs1(nums []int) int {
+	if len(nums) == 0 || len(nums) == 1 {
+		return 0
+	}
+
+	sum := 0
+
+	for i := len(nums) - 2; i >= 0; i-- {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				sum++
+			}
+		}
+	}
+	return sum
+}
+
+// 归并排序：
+func reversePairs(nums []int) int {
+	return mergeSort(nums, 0, len(nums)-1)
+}
+
+func mergeSort(nums []int, left, right int) int {
+	if left >= right {
+		return 0
+	}
+
+	// 递归
+	mid := (left + right) / 2
+	res := mergeSort(nums, left, mid) + mergeSort(nums, mid+1, right)
+
+	i, j := left, mid+1
+	temp := []int{}
+	// 计数
+	for i <= mid && j <= right {
+		if nums[i] <= nums[j] { // 注意符号，<=
+			temp = append(temp, nums[i])
+			res += j - (mid + 1) // 注意计数
+			i++
+		} else {
+			temp = append(temp, nums[j])
+			j++
+		}
+	}
+	// 处理剩余
+	for i <= mid {
+		temp = append(temp, nums[i])
+		res += right - mid
+		i++
+	}
+	for j <= right {
+		temp = append(temp, nums[j])
+		j++
+	}
+	// 覆盖
+	for i := 0; i < len(temp); i++ {
+		nums[left+i] = temp[i]
+	}
+	return res
+}
