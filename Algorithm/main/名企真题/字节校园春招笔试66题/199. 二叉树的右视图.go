d6aeffba23bf3afafed410b9a6f37f61eaac4a2e
YTIANYE <13293209977@163.com>
2022-03-10 14:28:17 +0800

    [90]199. 二叉树的右视图

--- Algorithm/main/名企真题/字节校园春招笔试66题/199. 二叉树的右视图.go
+++ Algorithm/main/名企真题/字节校园春招笔试66题/199. 二叉树的右视图.go
@@ -33,6 +33,37 @@ import (
 	. "awesomeProject1/Algorithm/main/structure/TreeNode"
 )
 
+// 我的题解：层次遍历
+
+/*
+执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+内存消耗：2.1 MB, 在所有 Go 提交中击败了99.82%的用户
+*/
+
 func rightSideView(root *TreeNode) []int {
+	if root == nil{
+		return []int{}
+	}
+
+	queue := []*TreeNode{root}
+	res := []int{}
+	for len(queue) != 0{
+
+		tqueue := queue
+		queue = []*TreeNode{}
+		res = append(res, tqueue[len(tqueue)-1].Val)
+		for len(tqueue) != 0{
+			node := tqueue[0]
+			tqueue = tqueue[1:]
+			if node.Left != nil{
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil{
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return res
 
 }
