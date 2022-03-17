b013ee1e16b470c0da4e0f73f288b379ad3126ba
YTIANYE <13293209977@163.com>
2022-03-17 16:22:50 +0800

    [108]394. 字符串解码

---
+++ Algorithm/main/名企真题/字节校园春招笔试66题/394. 字符串解码.go
@@ -0,0 +1,97 @@
+/*
+
+给定一个经过编码的字符串，返回它解码后的字符串。
+
+编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+
+你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+
+此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+
+ 
+
+示例 1：
+
+输入：s = "3[a]2[bc]"
+输出："aaabcbc"
+示例 2：
+
+输入：s = "3[a2[c]]"
+输出："accaccacc"
+示例 3：
+
+输入：s = "2[abc]3[cd]ef"
+输出："abcabccdcdcdef"
+示例 4：
+
+输入：s = "abc3[cd]xyz"
+输出："abccdcdcdxyz"
+ 
+
+提示：
+
+1 <= s.length <= 30
+s 由小写英文字母、数字和方括号 '[]' 组成
+s 保证是一个 有效 的输入。
+s 中所有整数的取值范围为 [1, 300] 
+
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/*
+执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+内存消耗：6 MB, 在所有 Go 提交中击败了8.11%的用户
+*/
+
+
+func decodeString(s string) string {
+
+	stack := []uint8{}
+	for i := range s{
+		if s[i] == ']'{
+			// 获取【】中的内容
+			tempstring := ""
+			for stack[len(stack)-1] != '['{
+				tempstring = string(stack[len(stack)-1]) + tempstring
+				stack = stack[:len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+			// 获取【】前面的数字
+			num := ""
+			for len(stack) >0 && stack[len(stack)-1] >= '0' && stack[len(stack)-1] <= '9'{
+				num = string(stack[len(stack)-1]) + num
+				stack = stack[:len(stack)-1]
+			}
+			numInt, _ := strconv.Atoi(num)
+			// 获取 数字*【】 的结果
+			tempres := ""
+			for ;numInt != 0;numInt--{
+				tempres += tempstring
+			}
+			// 新的字符串装入栈中
+			for i := range tempres{
+				stack = append(stack, tempres[i])
+			}
+		}else{
+			stack = append(stack, s[i])
+		}
+	}
+
+	res := ""
+	for i := range stack{
+		res += string(stack[i])
+	}
+	return res
+}
+
+func main(){
+	s := "3[a]2[bc]"
+	fmt.Println(decodeString(s))
+}
