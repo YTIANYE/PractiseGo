885a6bc9e3b0982abca92d5614bede29558d71ff
YTIANYE <13293209977@163.com>
2022-03-30 16:16:26 +0800

    [129]142. 环形链表 II

--- Algorithm/main/字节校园春招笔试66题/160. 相交链表.go
+++ Algorithm/main/字节校园春招笔试66题/160. 相交链表.go
@@ -97,3 +97,25 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	}
 	return nil
 }
+
+// 官方题解：更简洁
+func getIntersectionNode1(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	pa, pb := headA, headB
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+	return pa
+}
+
